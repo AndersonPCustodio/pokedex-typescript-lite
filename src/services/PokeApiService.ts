1af9a92f2c29d8040e9fc8ddf157f77e498108c1
Anderson Custodio <andy-custodio@hotmail.com>
2026-06-07 17:01:30 -0300

    feat: implementado busca e mapeamento de dados da pokeapi com tratamento de erros

---
+++ src/services/PokeApiService.ts
@@ -0,0 +1,34 @@
+import { PokemonApiResponse, PokemonResumo } from '../models/types.js';
+
+export async function buscarPokemon(nomeOuId: string): Promise<PokemonResumo | null> {
+  const url = `https://pokeapi.co{nomeOuId.toLowerCase().trim()}`;
+
+  //Bloco try/catch para capturar falhas de rede ou Pokémon inexistente
+  try {
+    const resposta = await fetch(url);
+
+    if (!resposta.ok) {
+      console.log(`[ERRO] Pokémon não encontrado: ${nomeOuId}`);
+      return null;
+    }
+
+    const dados: PokemonApiResponse = await resposta.json();
+
+    const tipos = dados.types.map((item) => item.type.name);
+
+    const pokemonSimplificado: PokemonResumo = {
+      id: dados.id,
+      nome: dados.name,
+      tipos: tipos,
+      altura: dados.height,
+      peso: dados.weight
+    };
+
+    console.log(`[OK] Pokémon encontrado: ${pokemonSimplificado.nome}`);
+    return pokemonSimplificado;
+
+  } catch (erro) {
+    console.log(`[ERRO] Não foi possível buscar o Pokémon.`);
+    return null;
+  }
+}
